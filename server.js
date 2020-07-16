9ee4840b1837ab9e44cb8039ae35f02b89123669
Mwansa <Mwansad.mwango@gmail.com>
2020-07-16 23:12:29 +0800

    Fixed depracation warnings in server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", { useNewUrlParser: true, useUnifiedTopology: true });
+// Added { useNewUrlParser: true, useUnifiedTopology: true } to remove Depracation warnings
 
 // Start the API server
 app.listen(PORT, function() {
