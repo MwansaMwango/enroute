e57d5dcbdf4b65ccb781f10ec45178186915bd9f
Mwansa <Mwansad.mwango@gmail.com>
2020-08-07 10:53:12 +0800

    Fixed jambotron to show on all pages

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -12,8 +12,8 @@ function Jumbotron({ children }) {
         color: "white",
         backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
           url(https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&h=250&q=80)
-          backgroundSize: "cover", // '100%' scales full image unlike 'cover'
           `,
+          backgroundSize: "cover", // '100%' scales full image unlike 'cover'
         backgroundRepeat: "no-repeat",
       }}
       className="jumbotron"
