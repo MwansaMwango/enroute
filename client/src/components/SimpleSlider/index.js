907637295c571c085ef7efb2d2b72ea89daa43b2
Mwansa <Mwansad.mwango@gmail.com>
2020-08-30 18:46:27 +0800

    Added auto play to newsfeed

--- client/src/components/SimpleSlider/index.js
+++ client/src/components/SimpleSlider/index.js
@@ -14,7 +14,7 @@ export default function SimpleSlider() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
+    autoplay: true,
     // pauseOnFocus: true,
   };
   return (
