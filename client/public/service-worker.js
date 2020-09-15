b69e4c71de6ad994543e8ba4d17813054abc2a5b
Mwansa <Mwansad.mwango@gmail.com>
2020-09-16 00:42:57 +0800

    Temp removed beams from index

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -3,16 +3,16 @@ importScripts("https://js.pusher.com/beams/service-worker.js");
 // You can now use the Service Worker SDK by calling
 // PusherPushNotifications.<METHOD_NAME>
 
-PusherPushNotifications.onNotificationReceived = ({
-  payload,
-  pushEvent,
-  handleNotification,
-}) => {
-  console.log("Payload", payload);
-  console.log("Payload Data", payload.data);
-  //   console.log("PushEvent", pushEvent);
-  //   console.log("HandleNotification", handleNotification);
-  // payload.notification.title = "A new title!";
-  // pushEvent.waitUntil(self.registration.showNotification('Auda Lee'));
-  pushEvent.waitUntil(handleNotification(payload));
-};
+// PusherPushNotifications.onNotificationReceived = ({
+//   payload,
+//   pushEvent,
+//   handleNotification,
+// }) => {
+//   console.log("Payload", payload);
+//   console.log("Payload Data", payload.data);
+//   //   console.log("PushEvent", pushEvent);
+//   //   console.log("HandleNotification", handleNotification);
+//   // payload.notification.title = "A new title!";
+//   // pushEvent.waitUntil(self.registration.showNotification('Auda Lee'));
+//   pushEvent.waitUntil(handleNotification(payload));
+// };
--- routes/api/pusher.js
+++ routes/api/pusher.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const PushNotifications = require("@pusher/push-notifications-server");
 const beamsClient = new PushNotifications({
-  // instanceId: "6af2ffd6-7acf-4ff5-9099-45bd1624be39",
-  // secretKey: "8B9AFA8838E2D7A271D34AF7407DF93B7F7514A1F80DDE49DB12AF8F845C4B3C",
-  instanceId: "0bb3f3ca-f205-4863-a264-e0e2264bc4bf",
-  secretKey: "7AAD3F24D5B9C2CDF0FF9093EE725E0D5270BD6B67ACD616CB77499FFE95E184",
+  instanceId: "6af2ffd6-7acf-4ff5-9099-45bd1624be39",
+  secretKey: "F094DE9101E2FA71FAECD085E50660325C9B7BA96424EBBB38F90E3277C9CFA3",
+  // instanceId: "0bb3f3ca-f205-4863-a264-e0e2264bc4bf",
+  // secretKey: "7AAD3F24D5B9C2CDF0FF9093EE725E0D5270BD6B67ACD616CB77499FFE95E184",
 });
 
 router.get("/beams-auth", function (req, res) {
